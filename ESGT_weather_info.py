f018b8272324449f9aabf33c1c89a2a7527ac5ea
Boa-Lin <blai7@gatech.edu>
2017-02-16 11:27:15 -0500

    updating max temp, min temp complete the json features

--- ESGT_weather_info.py
+++ ESGT_weather_info.py
@@ -3,13 +3,13 @@ python 2.7.10
 
 Author Boa-Lin Lai
 
-module install (pyowm):
 
 protopye --
 
 resouce from:
-  https://github.com/csparpa/pyowm/blob/master/pyowm/docs/usage-examples.md
-  http://openweathermap.org/
+  http://api.openweathermap.org/data/2.5/weather?id=4180439&mode=json&units=imperial&APPID=
+  http://api.openweathermap.org/data/2.5/forecast?id=4180439&mode=json&units=imperial&APPID=
+  http://opeinweathermap.org/
 
 '''
 import datetime
@@ -29,7 +29,9 @@ class ESGT_weather_info:
           int(time)
       ).strftime('%I:%M %p')
       return converted_time
-
+    
+    def set_city_id(self, new_id):
+      self.city_id = new_id
     def url_builder(self, api):
       '''
         current weather url builder, use differt api data 
@@ -37,9 +39,7 @@ class ESGT_weather_info:
       unit = 'imperial'  # For Fahrenheit use imperial, for Celsius use metric, and the default is Kelvin.
       # Search for your city ID here: http://bulk.openweathermap.org/sample/city.list.json.gz
       full_api_url = api + str(self.city_id) + '&mode=json&units=' + unit + '&APPID=' + self.api_key
-      print full_api_url
       return full_api_url
-
     def data_fetch(self,full_api_url):
         url = urlopen(full_api_url)
         # output = url.read().decode('utf-8')
@@ -48,35 +48,39 @@ class ESGT_weather_info:
         #raw_api_dict = json.loads(url.read())
         url.close()
         return raw_api_dict
-
     def get_json(self):
       current_weather_data = self.data_fetch(self.url_builder(self.current_weather_api))
       forecast_weather_data = self.data_fetch(self.url_builder(self.forecast_weather_api))
       # we only need the first day forcast
-      #for day in forecast_weather_data: # the first day dat
-      #  # is day a dictonary?
-      day_list =  forecast_weather_data['list'][:8]
-      #day_data = json.loads(forecast_weather_data['list'])
-      for l in day_list:
-        print 1
       data = self.data_organizer(current_weather_data,forecast_weather_data)
       json_array = json.dumps(data)
-      print json_array
       return json_array
 
     def data_organizer(self,raw_current_api_dict,raw_forecast_api_dict):
         '''
           This part revised the dictonary content
         '''
+        day_list =  raw_forecast_api_dict['list'][:8]
+        temp_max_list = []
+        temp_min_list = []
+        rain_stat = 0
+        for day_dict in day_list:
+          # 8 data for a day 24 hrs /3 hrs = 8 data
+          temp_max_list.append(day_dict.get('main').get('temp_max'))
+          temp_min_list.append(day_dict.get('main').get('temp_min'))
+          if(day_dict.get('rain')): rain_stat = day_dict.get('rain')
+          #potential bug
         data = dict(
           city=raw_current_api_dict.get('name'), # same
           country=raw_current_api_dict.get('sys').get('country'),
           temp=raw_current_api_dict.get('main').get('temp'),
-          temp_max=raw_current_api_dict.get('main').get('temp_max'),
-          temp_min=raw_current_api_dict.get('main').get('temp_min'),
+          temp_max=max(temp_max_list),
+          temp_min=min(temp_min_list), 
+          rain = rain_stat,
           humidity=raw_current_api_dict.get('main').get('humidity'),
           pressure=raw_current_api_dict.get('main').get('pressure'),
           sky=raw_current_api_dict['weather'][0]['main'],
+          icon=raw_current_api_dict.get('weather')[0].get('icon'),
           sunrise=self.time_converter(raw_current_api_dict.get('sys').get('sunrise')),
           sunset=self.time_converter(raw_current_api_dict.get('sys').get('sunset')),
           wind=raw_current_api_dict.get('wind').get('speed'),
@@ -85,23 +89,6 @@ class ESGT_weather_info:
           cloudiness=raw_current_api_dict.get('clouds').get('all')
         )
         return data
-def data_output(data):
-    m_symbol = '\xb0' + 'C'
-    print('---------------------------------------')
-    print('Current weather in: {}, {}:'.format(data['city'], data['country']))
-    print(data['temp'], m_symbol, data['sky'])
-    print('Max: {}, Min: {}'.format(data['temp_max'], data['temp_min']))
-    print('')
-    print('Wind Speed: {}, Degree: {}'.format(data['wind'], data['wind_deg']))
-    print('Humidity: {}'.format(data['humidity']))
-    print('Cloud: {}'.format(data['cloudiness']))
-    print('Pressure: {}'.format(data['pressure']))
-    print('Sunrise at: {}'.format(data['sunrise']))
-    print('Sunset at: {}'.format(data['sunset']))
-    print('')
-    print('Last update from the server: {}'.format(data['dt']))
-    print('---------------------------------------')
-
 
 if __name__ == '__main__':
     try:
