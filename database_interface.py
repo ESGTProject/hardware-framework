cd0b451adb80bd60546f77c1642750df3bcbe0bb
Kairi Kozuma <kairikozuma@gmail.com>
2017-02-16 13:05:45 -0500

    Fix non-persistence by adding commit, correct implementation to both current and backlog

--- database_interface.py
+++ database_interface.py
@@ -4,7 +4,7 @@
 File name: database_interface.py
 Author: Kairi Kozuma
 Date created: 02/08/2017
-Date last modified: 02/08/2017
+Date last modified: 02/16/2017
 Python Version: 2.7.11
 '''
 
@@ -14,8 +14,8 @@ import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from datetime import datetime
 import random
-# import pgpasslib #TODO need password?
-# password = pgpasslib.getpass('localhost', 5432, 'postgres', 'postgres')
+import json
+# import pgpasslib #TODO need password file if not default user
 
 # Formatting strings
 conn_template = "host='{}' dbname='{}' user='{}'"
@@ -46,16 +46,17 @@ COL_SENSORS_CREATE_TIME = 'create_time'
 # Connection strings used by psycopg2
 CONN_DEFAULT = conn_template.format(HOST_LOCALHOST, DB_POSTGRES, USER_POSTGRES)
 
+
 # Check connection to database
 def connect_database(db_name):
-    conn_db = conn_template.format(HOST_LOCALHOST, db_name, USER_POSTGRES);
+    conn_db = conn_template.format(HOST_LOCALHOST, db_name, USER_POSTGRES)
     try:
-        conn = psycopg2.connect(conn_db);
-        return conn
+        return psycopg2.connect(conn_db)
     except psycopg2.Error as e:
         print "Unable to connect to the database {}".format(db_name)
         print e
         return None
+
 
 # Create database 'db_name' if it does not exist
 def create_database(db_name):
@@ -65,6 +66,8 @@ def create_database(db_name):
         with conn.cursor() as cur:
             query = 'CREATE DATABASE {}'.format(db_name)
             cur.execute(query)
+        conn.commit()
+
 
 # Delete database given by 'db_name'
 def delete_database(db_name):
@@ -72,57 +75,93 @@ def delete_database(db_name):
         conn.autocommit = True
         with conn.cursor() as cur:
             query = 'DROP DATABASE IF EXISTS {}'.format(db_name)
-            cur.execute(query);
+            cur.execute(query)
+        conn.commit()
+
 
 # Clears contents of a given table 'table_name'
 def clear_table(conn, table_name):
     with conn.cursor() as cur:
         query = 'TRUNCATE TABLE {}'.format(table_name)
         cur.execute(query)
+    conn.commit()
 
 
 # ESGT specific functions
 # Create sensor table
 def create_sensor_table(conn):
-    with conn.cursor() as cur: #TODO: Separate tables for numerics and long strings?
-        query = 'CREATE TABLE IF NOT EXISTS {} ( \
-            "{}" SERIAL primary key, \
-            "{}" VARCHAR(50),        \
-            "{}" TEXT,               \
-            "{}" TIMESTAMP           \
+    with conn.cursor() as cur:
+        query = 'CREATE TABLE IF NOT EXISTS PUBLIC.{} (    \
+            {} SERIAL NOT NULL PRIMARY KEY,  \
+            {} VARCHAR(50) UNIQUE NOT NULL,  \
+            {} JSON NOT NULL,                \
+            {} TIMESTAMP NOT NULL            \
             )'.format(TABLE_SENSORS,
                 COL_SENSORS_ID,
                 COL_SENSORS_NAME,
                 COL_SENSORS_VALUE,
                 COL_SENSORS_CREATE_TIME)
         cur.execute(query)
+    conn.commit()
+
 
 # Create backlog table
 def create_backlog_table(conn):
     with conn.cursor() as cur:
-        query = 'CREATE TABLE IF NOT EXISTS {} ( \
-            "{}" SERIAL primary key,                \
-            "{}" bigint NOT NULL REFERENCES {}({}), \
-            "{}" TEXT,                              \
-            "{}" TIMESTAMP                          \
+        query = 'CREATE TABLE IF NOT EXISTS PUBLIC.{} (          \
+            {} SERIAL NOT NULL PRIMARY KEY,        \
+            {} INTEGER NOT NULL REFERENCES {}({}), \
+            {} JSON NOT NULL,                      \
+            {} TIMESTAMP NOT NULL                  \
             )'.format(TABLE_BACKLOG,
                 COL_BACKLOG_ID,
                 COL_BACKLOG_SENSOR_KEY, TABLE_SENSORS, COL_SENSORS_ID,
                 COL_BACKLOG_VALUE,
                 COL_BACKLOG_CREATE_TIME)
         cur.execute(query)
+    conn.commit()
 
 # Insert sensor 'sensor' and 'value' to the database, along with timestamp
-# Simulatenously inserts it into 'backlog' table and 'sensors' table
+# Simultaneously inserts it into 'backlog' table and 'sensors' table
 def insert_sensor_data(conn, sensor, value):
     with conn.cursor() as cur:
-        query = "INSERT INTO {} ({},{},{}) VALUES (%s, %s, %s)".format(
+        # Get time stamp
+        timestamp_now = datetime.now()
+
+        # Insert into current sensors table #TODO: Clean up text formatting references
+        query = "INSERT INTO {} ({},{},{}) VALUES (%s, %s, %s)\
+                 ON CONFLICT ({}) DO UPDATE SET {} = (%s), {} = (%s) ".format(
             TABLE_SENSORS,
             COL_SENSORS_NAME,
             COL_SENSORS_VALUE,
+            COL_SENSORS_CREATE_TIME,
+            COL_SENSORS_NAME,
+            COL_SENSORS_VALUE,
             COL_SENSORS_CREATE_TIME)
-        data = (sensor, value, datetime.now());
+
+        data = (sensor, value, timestamp_now, value, timestamp_now) #TODO: Fix duplicates
         cur.execute(query, data)
+
+        # Get sensor key id
+        query = "SELECT {} FROM {} WHERE {} = %s".format(
+            COL_SENSORS_ID,
+            TABLE_SENSORS,
+            COL_SENSORS_NAME)
+        data = (sensor,)
+        cur.execute(query, data)
+        id = cur.fetchall()[0] # Get id of foreign key
+
+        # Insert into backlog table
+        query = "INSERT INTO {} ({},{},{}) VALUES (%s, %s, %s)".format(
+            TABLE_BACKLOG,
+            COL_BACKLOG_SENSOR_KEY,
+            COL_BACKLOG_VALUE,
+            COL_BACKLOG_CREATE_TIME)
+        data = (id, value, timestamp_now)
+        cur.execute(query, data)
+
+    conn.commit()
+
 
 # Retrieve value for sensor name
 def select_sensor_data(conn, sensor):
@@ -134,14 +173,8 @@ def select_sensor_data(conn, sensor):
         data = (sensor,)
         cur.execute(query, data)
         rows = cur.fetchall()
-    return rows;
-
-# def delete_sensor_data(name):
-#     with psycopg2.connect(db_esgt) as con:
-#         with con.cursor() as cur:
-#             query = 'DELETE from SENSOR_DATA where name = (%s)'
-#             cur.execute(query, (name,)) #TODO: Fix redundancy
-#             print "Total number of rows deleted :", cur.rowcount
+    conn.commit()
+    return rows
 
 
 def main():
@@ -150,7 +183,7 @@ def main():
     db_name = DB_ESGT
 
     # Delete database
-    delete_database(db_name)
+    #delete_database(db_name)
 
     # Connect to database
     conn = connect_database(db_name)
@@ -170,27 +203,29 @@ def main():
             print e
             quit()
 
-    # Check if tables are present
+    # Create tables if not present
     create_sensor_table(conn)
     create_backlog_table(conn)
 
-    print "Press enter to insert 10 random entries and retrieve them\n"
+    # Retrieve data from table TODO: User dictionary cursor?
+    rows = select_sensor_data(conn, 'light_sensor')
+    print "Total entries: ", len(rows)
+
+    # Test inserting sensor values
+    sensor_names = ['light_sensor', 'temp_sensor', 'proximity_sensor']
+    print "Insert 10 random entries and retrieve them\n"
     for i in range(10):
-        insert_sensor_data(conn, 'light_sensor', 10 * i + random.uniform(0, 5.0))
+        for sen in sensor_names:
+            randval = 10 * i + random.uniform(0, 5.0)
+            jsonstr = json.dumps(['data', {'value': randval}])
+            insert_sensor_data(conn, sen, jsonstr)
 
-    raw_input();
-    # Retrieve data from table TODO: User dictionary cursor?
-    rows = select_sensor_data(conn, 'light_sensor');
+    rows = select_sensor_data(conn, 'light_sensor')
     for row in rows:
        print "value = ", row[0]
        print "timestamp = ", row[1], "\n"
 
-    raw_input();
-    # Retrieve data from table TODO: User dictionary cursor?
-    rows = select_sensor_data(conn, 'light_sensor');
-    for row in rows:
-       print "value = ", row[0]
-       print "timestamp = ", row[1], "\n"
+    print "Total entries: ", len(rows)
 
 if __name__ == "__main__":
     main()
