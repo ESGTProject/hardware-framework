2eda9b0ee89fc212049169f4a72c112d3aaaa0c7
Kairi Kozuma <kairikozuma@gmail.com>
2017-03-05 23:22:05 -0500

    Add doctring and use full flask app

--- app/app.py
+++ app/app.py
@@ -8,8 +8,7 @@ Date last modified: 03/01/2017
 Python Version: 2.7.11
 '''
 
-from flask import Flask, request
-from flask_restful import Resource, Api
+from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 import sqlalchemy
 import json
@@ -23,7 +22,6 @@ from ESGT_data import gmail #TODO: Cache in database?
 
 # Set up flask app variables
 app = Flask(__name__)
-api = Api(app)
 CORS(app) # Enable CORS
 
 # Default number of items to return
@@ -36,57 +34,40 @@ db_helper = None
 nondb_resource_dict = {}
 
 def date_handler(obj):
+    """Handles serialization of datetime objects
+
+    Default json serialization for Flask does not implement datetime
+    serialization, so this handler is passed to the argument for json.dumps()
+
+    Returns:
+        object serialized to isoformat (datetime object serialization)
+    """
     if hasattr(obj, 'isoformat'):
         return obj.isoformat()
     else:
         raise TypeError
 
 def flatten(row):
+    """Adds timestamp to same row as json object stored in row.value
+
+    Arguments:
+        row : Row returned from query into database
+            row.val is the json object
+            row.time_created is the time stamp
+
+    Returns:
+        Dictionary with the timestamp flattened into the json object at row.value
+    """
     dct = json.loads(row.value)
     dct["timestamp"] = pytz.utc.localize(row.time_created, is_dst=None).astimezone(pytz.utc)
     return dct
 
-class Resource(Resource):
-    def get(self, resource):
-        # If it is a nondatabase resource, attempt query
-        if resource in nondb_resource_dict.keys():
-            args = request.args
-            return json.loads(json.dumps(nondb_resource_dict[resource].get(args), default=date_handler))
-
-        else: # Use database to build response
-            limit_str = request.args.get('limit')
-            limit = int(limit_str) if limit_str is not None else DEFAULT_RESOURCE_LENGTH
-            rows = db_helper.select(resource, limit)
-            if rows is not None:
-                elem_array = list(map(flatten, rows))
-                return json.loads(json.dumps(elem_array, default=date_handler))
-
-    def put(self, resource):
-        return
-
-class ResourceList(Resource):
-    def get(self):
-        # List of resources from database
-        list_of_lists = db_helper.select_resources()
-        resource_list = [v for sublist in list_of_lists for v in sublist] # Flatten list of lists
-        # Add list of resources from non database resources
-        resource_list.extend(nondb_resource_dict.keys())
-        return resource_list
-
-    def put(self):
-        return
-
-class SimpleRestfulResource(object):
+class NewsAPIResource(object): #TODO: Move to own file in module
     def __init__(self, name, endpoint, api_key_query, api_key):
         self.name = name
         self.endpoint = endpoint
         self.api_key_query = api_key_query
         self.api_key = api_key
-    def get(self, params = {}):
-        raise NotImplementedError("subclasses must override get()")
-
-
-class NewsAPIResource(SimpleRestfulResource): #TODO: Move to own file in module
     def get(self, params = {}):
         params = params.to_dict()
         params[self.api_key_query] = self.api_key # add api key to params
@@ -106,11 +87,33 @@ class GmailAPIResource(object): #TODO: Move to own file in module
 
 
 # Add api endpoints
-api.add_resource(Resource, '/resource/<string:resource>')
-api.add_resource(ResourceList, '/resource')
+@app.route('/resource/<string:resource>', methods=['GET'])
+def get(self, resource):
+    # If it is a nondatabase resource, attempt query
+    if resource in nondb_resource_dict.keys():
+        args = request.args
+        # return json.loads(json.dumps(nondb_resource_dict[resource].get(args), default=date_handler))
+        return jsonify(nondb_resource_dict[resource].get(args))
+
+    else: # Use database to build response
+        limit_str = request.args.get('limit')
+        limit = int(limit_str) if limit_str is not None else DEFAULT_RESOURCE_LENGTH
+        rows = db_helper.select(resource, limit)
+        if rows is not None:
+            elem_array = list(map(flatten, rows))
+            return json.loads(json.dumps(elem_array, default=date_handler))
+
+@app.route('/resource', methods=['GET'])
+def get_resources():
+    # List of resources from database
+    list_of_lists = db_helper.select_resources()
+    resource_list = [v for sublist in list_of_lists for v in sublist] # Flatten list of lists
+    # Add list of resources from non database resources
+    resource_list.extend(nondb_resource_dict.keys())
+    return jsonify(resource_list)
+
 
 if __name__ == '__main__':
-
     # Instantiate database
     host = 'postgres'
     user = 'postgres'
@@ -133,4 +136,3 @@ if __name__ == '__main__':
     # Run micro server
     app.run(debug=True, host="0.0.0.0", port=8000)
     #app.run(debug=True, host="localhost", port=8000)
-
