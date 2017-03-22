02e2bf8e2ca79e17b94f3a10bb6f2d9dc7036aa7
itatchi42 <rayjosay@Yahoo.com>
2017-03-22 02:10:03 -0400

    main.cpp
    Added code to account for newly acquired humidity / temp and IR sensors.
    Should work perfectly, assuming sensors are wired correctly.

--- mbed/main.cpp
+++ mbed/main.cpp
@@ -1,58 +1,57 @@
 #include "mbed.h"
 #include "Dht11.h"
 
-/*
-Uncomment when sonar implemented
-void dist(int distance) // gets called by 'mu' every 0.1 sec
-{
-   distance *= 0.00328084f; // conv dist mm -> feet
-}
-ultrasonic sonar(p6, p7, .1, 1, &dist); // p6 = OUTPUT, p7 = CLOCK
-*/
+
 DigitalOut myled(LED1);
 Serial pc(USBTX, USBRX);
+Ticker tick;
+float valIR = 0;
+
 AnalogIn light(p15);
-//DHT humid(p16,SEN11301P);
+//DHT humid(p18,SEN11301P);
 Dht11 humid(p16);
-AnalogIn sensor3(p17);
+AnalogIn IR(p17); // Sharp IR 10 - 80 cm
 AnalogIn sensor4(p18);
 AnalogIn sensor5(p19);
 AnalogIn sensor6(p20);
 
-/*
-Uncommet when sonar implemented
-
-float g_dist; // for sonar
-*/
+void averageIR()
+{ // 1 sec total time
+    float val = 0;
+    for (int i = 0; i < 10; i++) {
+        // sum of 10 samples
+        val += IR.read();
+        wait (0.1);
+    }
+    // take avg. Scale from 0.0 - 1.0
+     valIR = (val / 10.0f) / 3.3f; // set global
+     return;  
+}
 
 int main() {
     // TO DO: Put this in a sleep to save energy
     char reqData;
-    /*
-    Uncomment when sonar implemented
-    
-    mu.startUpdates();//start measuring the distance
-    */
+    // call every 5 sec
+    tick.attach(&averageIR, 5.0); 
     while(1) {
       // if RPi requests data
       //bool working_flag = false;
-      if (pc.readable()) {
-      reqData = pc.getc(); // blocking
-      if (reqData == '0') {        
-      // UncommmenT when HTU21D Humidity / Temp Sensor purchased
-        pc.printf("Light,  Humidity,  Temperature (°F),  DISCONNECTED,  DISCONNECTED,  DISCONNECTED");      
-      }
-      else if (reqData == '1') {
+        if (pc.readable()) {
+            reqData = pc.getc(); // blocking
+            if (reqData == '0') {        
+                pc.printf("Light,  Humidity,  Temperature (°F),  DISCONNECTED,  DISCONNECTED,  DISCONNECTED");      
+            }
+            else if (reqData == '1') {
+            
+            //TO DO :  Refer to page 5 of  http://www.sharp-world.com/products/device/lineup/data/pdf/datasheet/gp2y0a21yk_e.pdf
+            // Use this for distance
         
-        //int  err = humid.readData(); // err is the status code 
-        //if (err == 0 || working_flag == true) {
-          //working_flag = true;
-          humid.read(); // update humidity + temp readings
-          pc.printf("%4.4f, %4.4f, %4.4f, %4.4f, %4.4f, %4.4f\n", light.read(), humid.getHumidity(), 1.0*humid.getFahrenheit(), sensor4.read(), sensor5.read(), sensor6.read());
-        } else {
-          pc.printf("Not printing humidity\n");
-        }   
-      }
+                humid.read(); // Required. Update humidity + temp readings
+                pc.printf("%4.4f, %4.4f, %4.4f, %4.4f, %4.4f, %4.4f\n", light.read(), humid.getHumidity(), humid.getFahrenheit(), (valIR / 3.3f), sensor5.read(), sensor6.read());
+            } else {
+                pc.printf("Not printing humidity\n");
+            }   
+        }
     }
-}
+} // end main
 
