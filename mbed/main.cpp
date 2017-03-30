88d4b3ffeaf9274ecfc3c7d3491176c74a5c123f
itatchi42 <rayjosay@Yahoo.com>
2017-03-30 03:31:51 -0400

    main.cpp
    Edited code to work with lower fidelity DHT11 humidity / temperature sensor. Was not getting correct readings when using the higher fidelity HTU21D-F humidity / temperature sensor. 
    
    Also accounted for IR sensor, which now reports reliable distance measurements. Turn screen on if object detected at <= 20 cm (When IR's output voltage is >= 1.25 / 3.3 V). Refer to the Sharp IR Sensor's datasheet here for calculations: https://www.sparkfun.com/datasheets/Components/GP2Y0A21YK.pdf

--- mbed/main.cpp
+++ mbed/main.cpp
@@ -1,56 +1,38 @@
 #include "mbed.h"
-#include "Dht11.h"
+//#include "HTU21D.h"
+#include "DHT11.h"
 
-
-DigitalOut myled(LED1);
 Serial pc(USBTX, USBRX);
-Ticker tick;
-float valIR = 0;
+float vcc = 3.3;
 
-AnalogIn light(p15);
-//DHT humid(p18,SEN11301P);
-Dht11 humid(p16);
-AnalogIn IR(p17); // Sharp IR 10 - 80 cm
+AnalogIn light(p15); // Light sensor
+DHT11 humid(p21); // pwmout... From: https://developer.mbed.org/users/s_inoue_mbed/code/DHT11_Hello_World/file/da7b1c04a659/main.cpp
+AnalogIn IR(p16); // Sharp IR 10 - 80 cm
 AnalogIn sensor4(p18);
 AnalogIn sensor5(p19);
 AnalogIn sensor6(p20);
 
-void averageIR()
-{ // 1 sec total time
-    float val = 0;
-    for (int i = 0; i < 10; i++) {
-        // sum of 10 samples
-        val += IR.read();
-        wait (0.1);
-    }
-    // take avg. Scale from 0.0 - 1.0
-     valIR = (val / 10.0f) / 3.3f; // set global
-     return;  
-}
 
 int main() {
-    // TO DO: Put this in a sleep to save energy
     char reqData;
-    // call every 5 sec
-    tick.attach(&averageIR, 5.0); 
+    int state;
     while(1) {
-      // if RPi requests data
-      //bool working_flag = false;
         if (pc.readable()) {
             reqData = pc.getc(); // blocking
             if (reqData == '0') {        
-                pc.printf("Light,  Humidity,  Temperature (°F),  DISCONNECTED,  DISCONNECTED,  DISCONNECTED");      
+                pc.printf("Light,  Humidity,  Temperature (°F),  ,  DISCONNECTED,  DISCONNECTED");      
             }
-            else if (reqData == '1') {
-            
-            //TO DO :  Refer to page 5 of  http://www.sharp-world.com/products/device/lineup/data/pdf/datasheet/gp2y0a21yk_e.pdf
-            // Use this for distance
-        
-                humid.read(); // Required. Update humidity + temp readings
-                pc.printf("%4.4f, %4.4f, %4.4f, %4.4f, %4.4f, %4.4f\n", light.read(), humid.getHumidity(), humid.getFahrenheit(), (valIR / 3.3f), sensor5.read(), sensor6.read());
-            } else {
-                pc.printf("Not printing humidity\n");
-            }   
+            else if (reqData == '1') {                        
+                state = humid.readData();
+                if (state != DHT11::OK) {
+                    pc.printf("ERROR\n");   
+                } 
+                else {   
+                    pc.printf("%4.4f, %d, %4.4f, %4.4f, %4.4f, %4.4f\n", light.read(), humid.readHumidity(), 32.0f + ((float)humid.readTemperature() * (9.0f / 5.0f)), IR.read(), sensor5.read(), sensor6.read());
+                    
+                }
+            } 
+            wait(3);
         }
     }
 } // end main
